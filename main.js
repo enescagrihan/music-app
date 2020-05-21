ca363d8742589647b251b668aeb90f84b5d6b2b0
kiremit <57963926+kiremit@users.noreply.github.com>
2020-05-21 05:33:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener('keydown', (e) => {
+    const tab = document.querySelector(`.pad[data-key="${e.keyCode}"]`);
+    const sound = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    if(!sound) return;
+    sound.play();
+    sound.currentTime = 0
+    tab.classList.add('playing');
+
+    
+    tab.ontransitionend = () => {
+        tab.classList.remove('playing');
+    }
+})
